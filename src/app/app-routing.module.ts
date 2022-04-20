512d53708bf548690ab605e9f2cbada98777e1e2
Rima <rymaessaidi@gmail.com>
2022-04-20 12:58:22 +0100

    search and articles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from "@angular/router";
 import { BoardAdminComponent } from "./board-admin/board-admin.component";
 import { AddUserComponent } from "./components/add-user/add-user.component";
 import { AlertsComponent } from "./components/alerts/alerts.component";
+import { ArticlesComponent } from "./components/articles/articles.component";
 import { ButtonsComponent } from "./components/buttons/buttons.component";
 import { ChipsComponent } from "./components/chips/chips.component";
 import { ExpansionComponent } from "./components/expansion/expansion.component";
@@ -60,6 +61,7 @@ const routes: Routes = [
       { path: "profile", component: ProfileComponent },
       { path: "admin", component: BoardAdminComponent },
       { path: "add", component: AddUserComponent },
+      { path: "articles", component: ArticlesComponent },
     ],
   },
 
